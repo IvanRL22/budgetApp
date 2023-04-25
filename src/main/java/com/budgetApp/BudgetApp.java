e60ed3c114ca7e3ef7c592b887357f16d98c9993
IvanRL22 <irecasensl@gmail.com>
2023-04-25 20:53:13 +0200

    Reorganize config files

--- src/main/java/com/budgetApp/BudgetApp.java
+++ src/main/java/com/budgetApp/BudgetApp.java
@@ -1,4 +1,4 @@
-package com.budgetApp.application;
+package com.budgetApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
